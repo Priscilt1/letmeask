2f0343ad7d4454328d541d4adfa1fb7887a800aa
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-06-24 08:28:29 -0300

    chore: :art: code indentation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,8 @@
-import { ButtonHTMLAttributes } from "react"
-import '../styles/button.scss'
+import { ButtonHTMLAttributes } from "react";
+import "../styles/button.scss";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> //pegando todos as propriedades que um bottao html tem. 
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>; //pegando todos as propriedades que um bottao html tem.
 
 export function Button(props: ButtonProps) {
-  return (
-    <button className="button" {...props}/>
-  )
+  return <button className="button" {...props} />;
 }
