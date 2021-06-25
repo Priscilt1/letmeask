f351d1b0f945cc3038af7e6f4f2a8b3120e8ae77
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-06-25 08:49:39 -0300

    feat: :construction: insert code

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,14 +1,22 @@
 import copy from "../assets/images/copy.svg"
 import '../styles/room-code.scss'
 
-export function RoomCode() {
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  }
+
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copy} alt="Copy room code"/>
       </div>
 
-      <span>Sala uhsuiahuishaiuha</span>
+      <span>Sala #{props.code}</span>
     </button>
   )
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,25 @@
+import { useParams } from 'react-router-dom'
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
 
 import "../styles/room.scss";
 
+//declarando quais parametros pode receber na rota
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
+  //armazenando os parametros
+  const params = useParams<RoomParams>()
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode />
+          <RoomCode code={params.id}/>
         </div>
       </header>
 
