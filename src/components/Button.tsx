83bc240eb33ea18993f5a1cfcea1a59757f37ffb
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-06-27 18:53:23 -0300

    feat: :lipstick: add isOutiline property to the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,13 @@
 import { ButtonHTMLAttributes } from "react";
 import "../styles/button.scss";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>; //pegando todos as propriedades que um bottao html tem.
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  //pegando todos as propriedades que um bottao html tem.
+  isOutlined?: boolean;
+};
 
-export function Button(props: ButtonProps) {
-  return <button className="button" {...props} />;
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
+  return (
+    <button className={`button ${isOutlined ? "outlined" : ""}`} {...props} />
+  );
 }
